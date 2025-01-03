c4e7478f7a641afd6ce2a2f5249ef651c51d2700
Santiago Cajal <s.cajalvarela@gmail.com>
2025-01-03 15:00:50 -0300

    chore: wip

---
+++ src/Contracts/Wizard.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * This file is part of the Wizardly package.
+ *
+ * (c) Santiago Cajal <s.cajalvarela@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Wizardly\Contracts;
+
+/**
+ * @internal
+ */
+interface Wizard
+{
+    //
+}
